8039cf3ebef1274ab601ad0e12ee47c98fa2650e
Jacob Gadikian <jacobgadikian@gmail.com>
2024-10-22 13:24:04 +0700

    lint...

--- app/app.go
+++ app/app.go
@@ -1061,8 +1061,8 @@ func (app *WasmApp) AutoCliOpts() autocli.AppOptions {
 }
 
 // DefaultGenesis returns a default genesis from the registered AppModuleBasic's.
-func (a *WasmApp) DefaultGenesis() map[string]json.RawMessage {
-	return a.BasicModuleManager.DefaultGenesis(a.appCodec)
+func (app *WasmApp) DefaultGenesis() map[string]json.RawMessage {
+	return app.BasicModuleManager.DefaultGenesis(app.appCodec)
 }
 
 // GetKey returns the KVStoreKey for the provided store key.
